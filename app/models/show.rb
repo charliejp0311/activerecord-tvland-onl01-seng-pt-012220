b41544cdd88a92202baea63bc6cce06a4d4ff5fb
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 20:09:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many :actors
+  has_many :actors, through: :characters
   belongs_to :network
+  
   def actors_list
-    (self.actors.map{|a| a.full_name}).join(", ")
+    
   end
+  
 end
