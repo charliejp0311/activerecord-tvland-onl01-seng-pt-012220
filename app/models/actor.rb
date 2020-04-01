612d2f69bebeed6a03798cb6920f779455846c23
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 19:12:03 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
   has_many :shows 
-  has_many :characters, through: :shows
+  has_many :characters
   
   def full_name
     "#{self.first_name} #{self.last_name}"
