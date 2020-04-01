503afd3998076df8c3857d39288f142742c33499
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 19:43:21 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,6 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    self.characters.map {|ts| "#{ts.name} - #{ts.show.name}"
+    self.characters.map {|ts| "#{ts.name} - #{ts.show.name}"}
   end
 end
