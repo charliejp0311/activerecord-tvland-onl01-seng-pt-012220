440c72d035ac2afc90db9699c64a7241a48e80af
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 19:30:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_184317) do
+ActiveRecord::Schema.define(version: 2020_04_01_183918) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_184317) do
     t.string "name"
     t.integer "actor_id"
     t.integer "show_id"
-    t.text "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
