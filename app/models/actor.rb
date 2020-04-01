53b5d8437bdde551461a3904eed7691ac95755ed
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 19:07:26 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
+  belongs_to :show 
+  belongs_to :character
   
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    self.characters.map { |c| c.name }
+  end
 end
